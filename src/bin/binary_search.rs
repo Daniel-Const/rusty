b0931facddc34ad0d8cfe65b8611a0cf0600c76f
Daniel Constantinidis <dancon98@hotmail.com>
2023-12-29 07:11:20 +1100

    add more examples + fix underflow on partition

--- src/bin/binary_search.rs
+++ src/bin/binary_search.rs
@@ -2,11 +2,17 @@ fn binary_search(vec: Vec<i32>, min: usize, max: usize, needle: i32) -> i32 {
     if min > max {
         return -1;
     }
-    let partition: usize = (min + max) / 2;
+    let partition: usize = min + (max - min) / 2;
     let value: i32 = vec[partition];
     return match value.cmp(&needle) {
         std::cmp::Ordering::Less    => binary_search(vec, partition+1, max, needle),
-        std::cmp::Ordering::Greater => binary_search(vec, min, partition-1, needle),
+        std::cmp::Ordering::Greater => {
+            // Prevent underflow on partition
+            if partition == 0 {
+                return -1;
+            }
+            return  binary_search(vec, min, partition-1, needle)
+        }
         std::cmp::Ordering::Equal   => partition.try_into().unwrap()
     }
 }
@@ -25,7 +31,12 @@ fn main() {
     let arr = vec![2, 4, 6, 8, 10, 11, 14, 15];
     perform_search(arr.clone(), 12);
     perform_search(arr.clone(), 14);
-
+    
     perform_search(rusty::generate_vec(20, 0, 100), 54);
+    perform_search(rusty::generate_vec(40, -100, 100), -80);
+    perform_search(rusty::generate_vec(60, 0, 1000), 63);
+    perform_search(rusty::generate_vec(100, 0, 10000), 8222);
+
+
 }
 
